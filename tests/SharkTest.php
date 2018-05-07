a89972d70e79bb74844a68e9fabedf41c9b7140f
AlbNor87 <albert.norberg@gmail.com>
2018-05-07 10:23:44 +0200

    test

--- tests/SharkTest.php
+++ tests/SharkTest.php
@@ -6,7 +6,7 @@
  * (c) Yrgo, högre yrkesutbildning.
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * file that was distributed with this source code..
  */
 
 declare(strict_types=1);
